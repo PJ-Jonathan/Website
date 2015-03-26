f7e4619778d63d49625f53519984f360bd96ff97
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 15:17:13 -0400

    Update makeAnAccount.php

--- php/makeAnAccount.php
+++ php/makeAnAccount.php
@@ -11,7 +11,7 @@ if($numRow==0){
   $sql = 'INSERT INTO users (name,email, password)
   VALUES ("'.$name.'","'.$email.'","'.$password.'")';
   if($conn->query($sql) or die(mysqli_error($conn))){
-  echo '<script type="text/javascript">window.location.href="/html/login.html"</script>';
+  echo '<script>window.location.href="/html/login.html"</script>';
   }else{
   echo "Unable To Submit Please Try Again";
   }
