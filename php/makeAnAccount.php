5ae240aee3090d46a6a5b363e3d3ede31f1e9dd3
root <root@ip-172-31-24-22.us-west-2.compute.internal>
2015-03-26 16:39:03 +0000

    Added Account System

--- php/makeAnAccount.php
+++ php/makeAnAccount.php
@@ -1,17 +1,14 @@
 <?php
-echo "Starting";
 require("db.php");
 $name = $_POST["name"];
 $email = $_POST["email"];
 $password = $_POST["password"];
-echo "Recieved POSTs";
 $password = hash("sha512",$password);
-echo " Hashed Passwords";
-$sql = 'INSERT INTO users (name,email, password, dob)
-VALUES ("'.$name.'","'.$email.'","'.$password.'","'$dob'")';
+$dob = 0;
+$sql = 'INSERT INTO users (name,email, password)
+VALUES ("'.$name.'","'.$email.'","'.$password.'")';
 if($conn->query($sql) or die(mysqli_error($conn))){
-echo '
-Success';
+echo 'Success';
 }else{
 echo "Unable To Submit Please Try Again";
 }
