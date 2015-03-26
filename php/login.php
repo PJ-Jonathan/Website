60503f4c26c9ad21e20cb0c4584999c2dbc729ba
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 16:56:38 -0400

    Update login.php

--- php/login.php
+++ php/login.php
@@ -3,7 +3,7 @@ require("db.php");
 $username = $_POST["username"];
 $password  = $_POST["pwd"];
 $password = hash("sha512",$password);
-$sql = 'SELECT * FROM users WHERE email="'.$username.'"';
+$sql = 'SELECT * FROM users WHERE email="'.$username.'" AND password ="'.$password.'"';
 $result = $conn->query($sql) or die(mysqli_error($conn));
 $numRow = $result->num_rows;
 if($numRow==1){//Failed
