1a6b2e747888e8beb83f0ae57a90fe9acc6ea290
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 09:57:14 -0400

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,1 +1,13 @@
-
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "test123";
+$dbname = "eBears_DB";
+$tablename = "Observations_1";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+?>
