1db212d421dcab52bf9a1f23b9e37d352fe21631
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 10:26:32 -0400

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,5 +1,17 @@
 <?php
-require(db.php);
+//require(db.php);
+$servername = "localhost";
+$username = "ebear";
+$password = "C4WfVQdEncPt4Ayp";
+$dbname = "eBears_DB";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Failed to Connect");
+}
+
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
