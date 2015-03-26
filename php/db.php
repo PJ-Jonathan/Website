04a60570b697b50a232016b74dd9f213ddb8a808
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 10:08:17 -0400

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,13 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "test123";
+$username = "ebear";
+$password = "C4WfVQdEncPt4Ayp";
 $dbname = "eBears_DB";
-$tablename = "Observations_1";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+die("Failed to Connect");
 }
 ?>
