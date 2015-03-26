39b7de3878e4b4fb27f60e45cbf8429d0fab0a02
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 12:16:48 -0400

    Update makeAnAccount.php

--- php/makeAnAccount.php
+++ php/makeAnAccount.php
@@ -1,7 +1,16 @@
 <?php
+require("db.php");
 $email = $_POST["email"];
 $password = $_POST["password"];
-echo $email.$password;
-
-
+$name = $_POST["name"];
+$dob = $_POST["dob"];
+$password = hash("sha512",$password);
+$sql = 'INSERT INTO users (name,email, password, dob)
+VALUES ("'.$name.'","'.$email.'","'.$password.'","'$dob'")';
+if($conn->query($sql) or die(mysqli_error($conn))){
+echo '
+Success';
+}else{
+echo "Unable To Submit Please Try Again";
+}
 ?>
