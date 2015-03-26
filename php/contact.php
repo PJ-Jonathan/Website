879e3500e82d351aa5082c212b6a67710b8056d0
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 10:28:32 -0400

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -17,7 +17,7 @@ $email = $_POST["email"];
 $message = $_POST["message"];
 
 $sql = 'INSERT INTO contact (name,email, message)
-VALUES ('.$name.','.$email.','.$message.')';
+VALUES ("'.$name.'","'.$email.'","'.$message.'")';
 
 if($conn->query($sql) or die(mysqli_error($conn))){
   echo "Submited";
