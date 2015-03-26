11f0c85214202878e04ad54b37a53238581d22cd
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-26 16:54:10 -0400

    Update login.php

--- php/login.php
+++ php/login.php
@@ -6,7 +6,6 @@ $password = hash("sha512",$password);
 $sql = 'SELECT * FROM users WHERE email="'.$username.'"';
 $result = $conn->query($sql) or die(mysqli_error($conn));
 $numRow = $result->num_rows;
-echo $numRows;
 if($numRow==1){//Failed
 echo "Auth";
 }else{
