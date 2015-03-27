00af4b7ba2d88ec547eba5ca67892e9592f4616b
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-27 09:10:45 -0400

    Update login.php

--- php/login.php
+++ php/login.php
@@ -6,8 +6,10 @@ $password = hash("sha512",$password);
 $sql = 'SELECT * FROM users WHERE email="'.$username.'" AND password ="'.$password.'"';
 $result = $conn->query($sql) or die(mysqli_error($conn));
 $numRow = $result->num_rows;
-if($numRow==1){//Failed
-echo "Auth";
+if($numRow==1){
+$key = str_shuffle("0123456789qwertyuiopasdfghjklzxcvbnm!@#$%^&*()?QWERTYUIOPZXCVBNM");
+$key = hash("sha512",$key);
+echo $key;
 }else{
 echo '<script>window.location.href="/html/login.html?wrongPass=true&&name='.$username.'"</script>';
 }
