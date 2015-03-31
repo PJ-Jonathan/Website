3b2102b274b4ae1b03bd5eb21f8bea0c60c03550
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-31 14:31:12 -0400

    Update submitObservation.php

--- php/submitObservation.php
+++ php/submitObservation.php
@@ -1,7 +1,7 @@
 <?php
  require("db.php");
  $date = $_POST["date"];
- $sql = 'INSERT INTO Observations_1 (date) VALUES ("1/1/1")';
+ $sql = 'INSERT INTO Observations_1 (date) VALUES ("'$date'")';
  $conn->query($sql) or die(mysqli_error($conn));
 
 
