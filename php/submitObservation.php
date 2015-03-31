b5a7bb73ec99a1dc4d88d60121fa93792cdeaccc
PJ Finlay <pjfin@users.noreply.github.com>
2015-03-31 15:01:46 -0400

    Update submitObservation.php

--- php/submitObservation.php
+++ php/submitObservation.php
@@ -1,10 +1,16 @@
 <?php
  require("db.php");
- $date = $_POST["date"];
+ /*$date = $_POST["date"];
  $time = $_POST["time"];
  $latitude = $_POST["latitude"];
  $longitude =$_POST["longitude"];
- $comments =$_POST["comments];
+ $comments =$_POST["comments];*/
+ $date = "1/1/1";
+ $time = "2:00";
+ $latitude = "1.2";
+ $longitude = "2.2";
+ $comments = "comment";
+ 
  $sql = 'INSERT INTO Observations_1 (date,time,latitude,longitude,comments) VALUES ("'.$date.'","'.$time.'","'.$latitude.'","'.$longitude.'","'.$comments.'")';
  $conn->query($sql) or die(mysqli_error($conn));
 
