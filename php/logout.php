a9c82518076554f01d9e5861f01093cc81bee920
PJ <pjfin123@gmail.com>
2015-04-02 19:39:26 -0400

    X

--- php/logout.php
+++ php/logout.php
@@ -1,25 +1,26 @@
 <?php
 session_start();
+require("db.php");
 echo "Started";
 require("accounts.php");
 echo "getting data";
 $username = $_SESSION["username"];
 $key = $_SESSION["key"];
-echo "About to check";
-	$username="pjfin123";
-	$key="dfsa";
-	$sql = 'SELECT * FROM users WHERE email="'.$username.'" AND password ="'.$key.'"';
-	echo "About to Query";	
-	$result = $conn->query($sql) or die(mysqli_error($conn));
-	echo "done Querying";
-	$numRow = $result->num_rows;
-	if($numRow==1){
-	    echo "True";
-	}else{
-		echo "False";
-	}
+$sql = 'SELECT * FROM users WHERE email="'.$username.'" AND userkey ="'.$key.'"';
+echo "About to Query";	
+$result = $conn->query($sql) or die(mysqli_error($conn));
+echo "done Querying";
+$numRow = $result->num_rows;
+if($numRow==1){
+    echo "True";
+}else{
+    echo "False";
+}
 
-//$sql = 'UPDATE users SET userkey="'.$key.'" WHERE email="'.$username.'"';
+
+
+
+$sql = 'UPDATE users SET userkey="'.$key.'" WHERE email="'.$username.'"';
 
 
 $_SESSION["username"] = "";
