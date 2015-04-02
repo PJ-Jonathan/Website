4426b9712677138ed8e330fa90219cc2fcae8263
PJ <pjfin123@gmail.com>
2015-04-02 13:22:28 -0400

    Hair Form

--- php/submitHair.php
+++ php/submitHair.php
@@ -5,7 +5,7 @@ $date = $_POST["date"];
 $time = $_POST["time"];
 $id = $_POST["siteid"];
 $sql = 'INSERT INTO hair_collection_data (email,date, time, site_id)
-VALUES ("'.$email.'","'.$date.'","'.$time.'","'.$site_id.'")';
+VALUES ("'.$email.'","'.$date.'","'.$time.'","'.$id.'")';
 if($conn->query($sql) or die(mysqli_error($conn))){
 echo '
 <script>window.location.href="../html/submissionComplete.html"</script>
