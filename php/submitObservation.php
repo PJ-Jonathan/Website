16eaf8c0112343760ff690e5990170dbb8a0e147
pjfin <pjfin123@gmail.com>
2015-04-02 12:09:26 -0400

    Working on POST

--- php/submitObservation.php
+++ php/submitObservation.php
@@ -1,15 +1,16 @@
 <?php
  require("db.php");
- /*$date = $_POST["date"];
+ $date = $_POST["date"];
+ echo $date;
  $time = $_POST["time"];
+ echo $tiem;
  $latitude = $_POST["latitude"];
+ echo $latitude;
  $longitude =$_POST["longitude"];
- $comments =$_POST["comments];*/
- $date = "1/1/1";
- $time = "2:00";
- $latitude = "1.2";
- $longitude = "2.2";
- $comments = "comment";
+ echo $longitude;
+ $comments =$_POST["comments"];
+ echo $comments;
+
  
  $sql = 'INSERT INTO Observations_1 (date,time,latitude,longitude,comments) VALUES ("'.$date.'","'.$time.'","'.$latitude.'","'.$longitude.'","'.$comments.'")';
  $conn->query($sql) or die(mysqli_error($conn));
