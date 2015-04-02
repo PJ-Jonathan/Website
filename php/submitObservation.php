cbff2dcda3590ce22e5df2fbdf3c9bbcc1fb75eb
PJ <pjfin123@gmail.com>
2015-04-02 16:42:41 -0400

    messing with file structure for upload

--- php/submitObservation.php
+++ php/submitObservation.php
@@ -3,7 +3,7 @@ require("db.php");
 
 
 
-$target_dir = "../upload";
+$target_dir = "/var/www/html/upload";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
