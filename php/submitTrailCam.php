fdb8dcb6e1965677533e9b466fb91cec967afccc
PJ <pjfin123@gmail.com>
2015-04-02 18:39:23 -0400

    bug fix

--- php/submitTrailCam.php
+++ php/submitTrailCam.php
@@ -56,12 +56,12 @@ $sdate = $_POST["sdate"];
 $stime = $_POST["stime"];
 $edate =$_POST["edate"];
 $etime =$_POST["etime"];
-$sightings = $_POST["sighting"];
+$sightings = $_POST["sightings"];
 $comments = $_POST["comments"];
 $latitude = $_POST["latitude"];
 $longitude = $_POST["longitude"];
 
-$sql = 'INSERT INTO trail_camera_data (email,start_date,start_time,end_date,end_time,sightings,latitude,longitude,comments,photo_url) VALUES ("'.$email.'","'.$sdate.'","'.$stime.'","'.$edate.'","'.$etime.'","'.$sightings.'","'.$latitude.'","'.$longitude.'","'.$comments.'","'.$picure_url.'")';
+$sql = 'INSERT INTO trail_camera_data (email,start_date,start_time,end_date,end_time,sightings,latitude,longitude,comments,photo_url) VALUES ("'.$email.'","'.$sdate.'","'.$stime.'","'.$edate.'","'.$etime.'","'.$sightings.'","'.$latitude.'","'.$longitude.'","'.$comments.'","'.$picture_url.'")';
 $conn->query($sql) or die(mysqli_error($conn));
 echo '<script>window.location.href="../html/submissionComplete.html"</script>'
 
