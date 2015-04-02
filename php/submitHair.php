e4cf370b727c5e4e1e60c4d31a5aaace5a3d6a31
PJ <pjfin123@gmail.com>
2015-04-02 13:26:55 -0400

    Added comments

--- php/submitHair.php
+++ php/submitHair.php
@@ -4,8 +4,9 @@ $email = $_POST["email"];
 $date = $_POST["date"];
 $time = $_POST["time"];
 $id = $_POST["siteid"];
-$sql = 'INSERT INTO hair_collection_data (email,date, time, site_id)
-VALUES ("'.$email.'","'.$date.'","'.$time.'","'.$id.'")';
+$comments = $_POST["comments"];
+$sql = 'INSERT INTO hair_collection_data (email,date, time, site_id, comments)
+VALUES ("'.$email.'","'.$date.'","'.$time.'","'.$id.'","'.$comments.'")';
 if($conn->query($sql) or die(mysqli_error($conn))){
 echo '
 <script>window.location.href="../html/submissionComplete.html"</script>
