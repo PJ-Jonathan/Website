9441cf89d1ef926a1adea2318ac3bf449913f987
PJ Finlay <pjfin@users.noreply.github.com>
2015-06-30 11:32:11 -0400

    Update submitObservation.php

--- php/submitObservation.php
+++ php/submitObservation.php
@@ -14,7 +14,7 @@ $comments =$_POST["comments"];
 $email = $_POST["email"];
 $type = $_POST["type"];
 $confidence = $_POST["confidence"];
-if(isset$_POST["picture_url"]) $picture_url = $_POST["picture_url"];
+if(isset($_POST["picture_url"])) $picture_url = $_POST["picture_url"];
 $sql = 'INSERT INTO Observations_1 (date,time,latitude,longitude,comments,email,type,picture_url,confidence) VALUES ("'.$date.'","'.$time.'","'.$latitude.'","'.$longitude.'","'.$comments.'","'.$email.'","'.$type.'","'.$picture_url.'","'.$confidence.'")';
 $conn->query($sql) or die(mysqli_error($conn));
 
